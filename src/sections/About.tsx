6564a9a90e9a2c679f05009af888174f9451f09b
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2026-05-14 18:23:31 +0600

    add tech stack icon

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -7,10 +7,11 @@ import Image from "next/image";
 import mapImage from '@/assets/images/dhaka-map.png'
 import smileMemoji from '@/assets/images/memoji-smile.png'
 import JavascriptIcon from "@/assets/icons/square-js.svg";
+import TypecriptIcon from "@/assets/icons/typescript.svg";
 import HtmlIcon from "@/assets/icons/html5.svg";
 import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
-import ChromeIcon from "@/assets/icons/chrome.svg";
+import NextJsIcon from "@/assets/icons/nextjs.svg";
 import GithubIcon from "@/assets/icons/github.svg";
 import { CardHeader } from '@/components/CardHeader'
 import { ToolboxItems } from "@/components/ToolboxItems";
@@ -18,10 +19,11 @@ import { useRef } from "react";
 
 const toolboxItems = [
     { title: 'JavaScript', iconType: JavascriptIcon },
+    { title: 'TypeScript', iconType: TypecriptIcon },
     { title: 'HTML5', iconType: HtmlIcon },
     { title: 'CSS3', iconType: CssIcon },
     { title: 'React', iconType: ReactIcon },
-    { title: 'Chrome', iconType: ChromeIcon },
+    { title: 'Next.Js', iconType: NextJsIcon },
     { title: 'Github', iconType: GithubIcon },
 ]
 
